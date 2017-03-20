998c0bb51e8032a695dce21e87b585806611b248
Joel Lord <joel.lord@hotmail.co.uk>
2017-03-20 14:06:25 -0400

    add LDR to load values into a register and refactor loop

--- vmvm.cpp
+++ vmvm.cpp
@@ -15,6 +15,7 @@ enum Instructions {
   SUB,
   MUL,
   DIV,
+  LDR,
 	HLT
 };
 
@@ -26,13 +27,24 @@ const std::unordered_map<std::string, Instructions> instruction_set {
   { "SUB", SUB },
   { "MUL", MUL },
   { "DIV", DIV },
+  { "LDR", LDR },
 	{ "HLT", HLT }
 };
 
 enum Registers {
 	A, B, C, D, E, F,
+  ACC,
 	SP, IP,
 	LEN
+};
+
+const std::unordered_map<std::string, Registers> register_set {
+  { "A", A },
+  { "B", B },
+  { "C", C },
+  { "D", D },
+  { "E", E },
+  { "F", F }
 };
 
 std::vector<int> stack;
@@ -50,77 +62,89 @@ std::vector<int> parse_file() {
 	}
 
 	std::vector<int> program_tokens;
-	std::for_each(begin(program_lines), end(program_lines), [&program_tokens](std::string s)
-	{
-		std::stringstream ss;
-		ss.str(s);
-		std::string token;
-		while (getline(ss, token, ' ')) {
-			if(instruction_set.find(token) != instruction_set.end()) {
-				program_tokens.push_back(instruction_set.at(token));
-			} else {
-				// TODO: validate
-				program_tokens.push_back(std::stoi(token));
-			}
-		}
-	});
 
-	return program_tokens;
+  for(auto current_line:program_lines) {
+    std::stringstream ss;
+    ss.str(current_line);
+    std::string token;
+    while(getline(ss, token, ' ')) {
+      if(instruction_set.find(token) != instruction_set.end()) {
+        program_tokens.push_back(instruction_set.at(token));
+      } else {
+        // TODO: Validate
+        if(program_tokens.back() == LDR) {
+          program_tokens.push_back(register_set.at(token));
+        } else {
+          program_tokens.push_back(std::stoi(token));
+        }
+      }
+    }
+  }
+
+  return program_tokens;
+}
+
+void dump_registers() {
+  for(auto& i:registers) {
+    printf("RegVal: %d\n", i);
+  }
 }
 
 int main()
 {
-	auto program = parse_file();
+  auto program = parse_file();
 
-	for(size_t ip=0; ip != program.size(); ++ip) {
-		switch (program[ip]) {
-		case PSH:
-			++ip;
-			stack.push_back(program[ip]);
-			break;
-		case ADD:
-			registers[B] = stack.back();
-			stack.pop_back();
-			registers[A] = stack.back();
-			stack.pop_back();
-			registers[C] = registers[A] + registers[B];
-			stack.push_back(registers[C]);
-			break;
-    case SUB:
-      registers[B] = stack.back();
-      stack.pop_back();
-      registers[A] = stack.back();
-      stack.pop_back();
-      registers[C] = registers[A] - registers[B];
-      stack.push_back(registers[C]);
-      break;
-    case MUL:
-      registers[B] = stack.back();
-      stack.pop_back();
-      registers[A] = stack.back();
-      stack.pop_back();
-      registers[C] = registers[A] * registers[B];
-      stack.push_back(registers[C]);
-      break;
-    case DIV:
-      registers[B] = stack.back();
-      stack.pop_back();
-      registers[A] = stack.back();
-      stack.pop_back();
-      registers[C] = registers[A] / registers[B];
-      stack.push_back(registers[C]);
-      break;
-		case POP:
-			registers[A] = stack.back();
-			stack.pop_back();
-			printf("%d\n", registers[A]);
-			break;
-		case HLT:
-			std::cin.get();
-			break;
-			default: ;
-		}
-	}
+  for(size_t ip=0; ip != program.size(); ++ip) {
+    switch (program[ip]) {
+      case PSH:
+        ++ip;
+        stack.push_back(program[ip]);
+        break;
+      case ADD:
+        registers[ACC] = stack.back();
+        stack.pop_back();
+        registers[ACC] = stack.back() + registers[ACC];
+        stack.pop_back();
+        stack.push_back(registers[ACC]);
+        break;
+      case SUB:
+        registers[ACC] = stack.back();
+        stack.pop_back();
+        registers[ACC] = stack.back() - registers[ACC];
+        stack.pop_back();
+        stack.push_back(registers[ACC]);
+        break;
+      case MUL:
+        registers[ACC] = stack.back();
+        stack.pop_back();
+        registers[ACC] = stack.back() * registers[ACC];
+        stack.pop_back();
+        stack.push_back(registers[ACC]);
+        break;
+      case DIV:
+        registers[ACC] = stack.back();
+        stack.pop_back();
+        registers[ACC] = stack.back() / registers[ACC];
+        stack.pop_back();
+        stack.push_back(registers[ACC]);
+        break;
+      case POP:
+        printf("POP: %d\n", stack.back());
+        stack.pop_back();
+        break;
+      case LDR:
+        ++ip;
+        registers[program[ip]] = program[ip+1];
+        ++ip;
+        break;
+      case HLT:
+        dump_registers();
+        std::cin.get();
+        exit(0);
+        break;
+      default: ;
+    }
+  }
 
-	return 0;
+  return 0;
 }
